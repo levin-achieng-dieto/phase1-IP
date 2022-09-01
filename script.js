bf4828c1532a9d319ad688db2736be7e72eede49
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-09-02 00:20:31 +0300

    working on post

--- script.js
+++ script.js
@@ -19,29 +19,38 @@ likebtn.addEventListener('click', () => {
 
 
 document.addEventListener("DOMContentLoaded", (e) => {
-    fetchingMembers()
+    // fetchingMembers()
     e.preventDefault()
     // fetching()
+    renderCat()
+
 })
 
-const names = document.querySelector("#names")
-const image = document.querySelector(".image")
-const age = document.querySelector("#age")
-const likes = document.querySelector(".like-button")
+// const names = document.querySelector("#names")
+// const image = document.querySelector(".image")
+// const age = document.querySelector("#age")
+// const likes = document.querySelector(".like-button")
 
-function renderMembers(cat){
-    names.textContent = cat.catname;
-    image.src = cat.image;
-    age.textContent = cat.age;
-}
+// function renderMembers(cat){
+//     const holder = document.createElement('div')
+//     names.textContent = cat.catname;
+//     image.src = cat.image;
+//     age.textContent = cat.age;
+
+//     // holder.append(names, image, age)
+
+//     // let rendered = document.getElementById('cards')
+//     // rendered =""
+//     // rendered.append(holder)
+// }
 
 
 
-function fetchingMembers(){
-    fetch('http://localhost:3000/cats')
-    .then(response => response.json())
-    .then(data => data.forEach(cat => renderMembers(cat)))
-}
+// // function fetchingMembers(){
+// //     fetch('http://localhost:3000/cats')
+// //     .then(response => response.json())
+// //     .then(data => data.forEach(cat => renderMembers(cat)))
+// // }
 
 
 // function fetching(){
@@ -70,32 +79,74 @@ function fetchingMembers(){
 // }
 
 
-document.querySelector('#reg-info').addEventListener('submit', handleSubmit)
+// // document.querySelector('#reg-info').addEventListener('submit', handleSubmit)
 
-const petName = document.querySelector('#pet_name')
-const petAge = document.querySelector('#pet_age')
-const petImage = document.querySelector('#pet_image')
+// // const petName = document.querySelector('#pet_name').textContent
+// // const petAge = document.querySelector('#pet_age').textContent
+// // const petImage = document.querySelector('#pet_image').textContent
 
+
+// // function handleSubmit(e){
+// //     e.preventDefault()
+// //     const catObject = {
+// //         catname:e.target.petName.value,
+// //         age:e.target.petAge.value,
+// //         image:e.target.petImage.value,
+// //         // id:0
+// //     }
+// //     addCats(catObject)
+// // }
+
+
+
+// // // const petid = document.querySelector('#pet_id').value
+
+
+
+// // function addCats(catObject){
+// //     console.log(JSON.stringify(catObject));
+// //     fetch('http://localhost:3000/cats', {
+// //         method: 'POST',
+// //         headers: {
+// //             'Content-Type': 'application/json'
+// //         },
+// //         body:JSON.stringify({catname: petName,
+// //         age:petAge,
+// //         image:petImage,
+// //         id:0})
+// //     })
+// //     .then(response => response.json())
+// //     .then(catObject => console.log(catObject))
+// // }
+
+
+
+
+
+let petname = document.querySelector('#pet_name').value
+let petage = document.querySelector('#pet_age').value
+let petimage = document.querySelector('#pet_image').value
+
+document.getElementById('reg-info').addEventListener('click', handleSubmit)
 
 function handleSubmit(e){
     e.preventDefault()
-    const catObject = {
-        catname:e.target.petName.value,
-        age:e.target.petAge.value,
-        image:e.target.petImage.value,
-        id:0
+    let catObject = {
+        catname:e.target.petname.value,
+        age:e.target.petage.value,
+        image:e.target.petimage.value
     }
-    addCats(catObject)
+    renderCat(catObject)
 }
 
-function addCats(catObject){
+function renderCat(catObject){
     fetch('http://localhost:3000/cats', {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Conteent-Type': 'application/json'
         },
         body:JSON.stringify(catObject)
     })
     .then(response => response.json())
-    .then(cat => console.log(cat))
-}
+    .then(data => console.log(data))
+    }
