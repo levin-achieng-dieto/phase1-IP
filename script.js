515c60747d989e5f29e8d637005fe2e002721cf9
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-09-01 15:36:47 +0300

    handling post

--- script.js
+++ script.js
@@ -18,11 +18,10 @@ likebtn.addEventListener('click', () => {
 })
 
 
-
 document.addEventListener("DOMContentLoaded", (e) => {
-    e.preventDefault()
-    // renderMembers()
     fetchingMembers()
+    e.preventDefault()
+    // fetching()
 })
 
 const names = document.querySelector("#names")
@@ -32,9 +31,8 @@ const likes = document.querySelector(".like-button")
 
 function renderMembers(cat){
     names.textContent = cat.catname;
-    image.textContent= cat.image;
+    image.src = cat.image;
     age.textContent = cat.age;
-    // likes.textContent = cat.likes
 }
 
 
@@ -43,4 +41,61 @@ function fetchingMembers(){
     fetch('http://localhost:3000/cats')
     .then(response => response.json())
     .then(data => data.forEach(cat => renderMembers(cat)))
+}
+
+
+// function fetching(){
+//     fetch('http://localhost:3000/cats')
+//     .then(response => response.json())
+//     .then(data => {
+//         const details = document.querySelector('#footer')
+//     data.forEach(cat => {
+//         const list = document.createElement('li')
+
+//         names.textContent = cat.catname;
+//         image.textContent= cat.image;
+//         age.textContent = cat.age;
+
+//         list.appendChild(names)
+//         list.appendChild(image)
+//         list.appendChild(age)
+//         console.log(list);
+//         const card =document.querySelector('.cards')
+//         card.append(list)
+//         // details.append(card)
+//         // details.append(list)
+
+//     })
+//     })
+// }
+
+
+document.querySelector('#reg-info').addEventListener('submit', handleSubmit)
+
+const petName = document.querySelector('#pet_name')
+const petAge = document.querySelector('#pet_age')
+const petImage = document.querySelector('#pet_image')
+
+
+function handleSubmit(e){
+    e.preventDefault()
+    const catObject = {
+        catname:e.target.petName.value,
+        age:e.target.petAge.value,
+        image:e.target.petImage.value,
+        id:0
+    }
+    addCats(catObject)
+}
+
+function addCats(catObject){
+    fetch('http://localhost:3000/cats', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify(catObject)
+    })
+    .then(response => response.json())
+    .then(cat => console.log(cat))
 }
